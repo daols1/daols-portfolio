d3debdb28f5e94e4a2bfc2b7174197c940c12394
David <78326757+daols1@users.noreply.github.com>
2023-02-02 17:08:00 +0100

    Installed router and react icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import './App.css';
+import Home from './Pages/Home/Home'
 
 function App() {
   return (
     <div className="App">
+      <Home />
     {/* Components get imported here */}
     </div>
   );
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Home() {
   return (
     <div>
-      
+      Hello just testing some shit out
     </div>
   )
 }
