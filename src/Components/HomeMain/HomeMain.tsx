beca02f350a15dd745045b78d965d903237da2f8
David <78326757+daols1@users.noreply.github.com>
2023-02-25 11:36:48 +0100

    Added main hero

--- src/Components/HomeMain/HomeMain.tsx
+++ src/Components/HomeMain/HomeMain.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
-import 'HomeMain.css'
+import './HomeMain.css';
+import { Link } from 'react-router-dom';
+import disppic from '../../asset/disppic.jpg'
 
 function HomeMain() {
   return (
-    <div className='container home-main-contain'>
-        
+    <div className='home-main-contain'>
+        <div className='h-m-txt'>
+            <h1>David Ladipo</h1>
+            <h1>Front-end Developer</h1>
+            <p>Welcome to my website! I am a Front-end Developer with a passion for creating beautiful and responsive websites.
+            
+            <br/>
+            <br/>
+
+            As a Front-end Developer, I specialize in the client-facing side of web development, ensuring that websites are both visually appealing and user-friendly. I use a combination of HTML, CSS, and JavaScript to create dynamic, interactive websites that engage users and deliver results.</p>
+            <Link to='' >Lets Talk 😁</Link>
+        </div>
+        <div className='h-m-img'>
+            <img src={disppic} alt="" className='h-img' />
+            <div className='floaters f1'></div>
+            <div className='floaters f2'></div>
+        </div>
     </div>
   )
 }
--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Footer from '../../Components/Footer/Footer';
+import HomeMain from '../../Components/HomeMain/HomeMain';
 import Menu from '../../Components/Main-menu/Menu';
 
 function Home() {
   return (
     <div className='container'>
-      <h1>THis is the home page</h1>
+      <HomeMain/>
     </div>
   )
 }
