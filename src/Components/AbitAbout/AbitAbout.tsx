f39488b1bcf7966094e23a7cb8b0494d48eeccb0
David <78326757+daols1@users.noreply.github.com>
2023-03-01 00:52:36 +0100

    God Abeg

--- src/Components/AbitAbout/AbitAbout.tsx
+++ src/Components/AbitAbout/AbitAbout.tsx
@@ -1,5 +1,5 @@
 import './AbitAbout.css'
-import secondPic from '../../asset/pictwo.jpg'
+import secondPic from '../../asset/pictwo2.jpg'
 import {Link} from 'react-router-dom'
 
 function AbitAbout() {
